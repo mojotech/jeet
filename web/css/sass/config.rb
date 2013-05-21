91b454ca1bcbff4e1ee413d5839d9df080cc6dc4
ozamorowski <ozamorowski@gmail.com>
2013-05-21 15:41:54 +0200

    jeet2 sass port

--- web/css/sass/config.rb
+++ web/css/sass/config.rb
@@ -20,3 +20,4 @@ output_style = :compressed
 preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+Sass::Script::Number.precision = 14
--- web/css/scss/config.rb
+++ web/css/scss/config.rb
@@ -7,7 +7,7 @@ sass_dir = "./"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = :compressed
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
