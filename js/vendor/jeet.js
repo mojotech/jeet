10486418dffd3e9c2417251574ebb3169fc6a408
CorySimmons <cory@pressedweb.com>
2013-09-29 04:12:04 -0400

    Add Midway.js; remove align()

--- js/vendor/jeet.js
+++ js/vendor/jeet.js
@@ -14,6 +14,9 @@ jQuery.fn.toggleAttr = function( name, onValue, offValue, on ) {
         });
 };
 
+// Vertical/horizontal responsive centering
+function Midway(){var e=$(".midway-horizontal"),t=$(".midway-vertical");e.each(function(){$(this).css("marginLeft",-$(this).outerWidth()/2)});t.each(function(){$(this).css("marginTop",-$(this).outerHeight()/2)});e.css({display:"inline",position:"absolute",left:"50%"});t.css({display:"inline",position:"absolute",top:"50%"})}$(window).on("load",Midway);$(window).on("resize",Midway);
+
 // Custom Jeet JS
 $(function() {
     
