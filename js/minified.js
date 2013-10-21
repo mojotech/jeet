d1681377b4b7178f108192b624f2c9b7d2fe2e98
CorySimmons <cory@pressedweb.com>
2013-10-20 22:00:09 -0400

    Fix mobile first typography; add settings.styl; remove palette.styl; fix reset.styl

--- js/minified.js
+++ js/minified.js
@@ -1,4 +1,1 @@
-// Want automatic minification, LiveReload, Coffeescript compiling, project creation?
-// https://github.com/CorySimmons/jeet-npm
-
 $(function(){});$(function(){$(".chromeframe").on("click",function(){$(this).slideUp("fast")})});
