376819b6187b53f22259c153de41c0cfce35f3f0
Cory Simmons <cory@mojotech.com>
2013-11-08 12:46:26 -0500

    Remove autoprefixer integration from Jeet and rename to standard module file name

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-var autoprefixer = require('autoprefixer-stylus');
-
 module.exports = function(opts) {
     var implicit = (opts && opts.implicit == false) ? false : true;
-    
     return function(style){
-        // Include Jeet
         style.include(__dirname);
-
-        // Implicit import handling
         if (implicit) style.import('jeet');
-
-        // Autoprefixer integration
-        autoprefixer()(style);
     }
 }
