f435251667f1ce6b4027c04e812d1512ad4e3088
Cory Simmons <cory@mojotech.com>
2013-11-08 17:11:01 -0500

    Fix jeet to index in index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = function(opts) {
     var implicit = (opts && opts.implicit == false) ? false : true;
     return function(style){
         style.include(__dirname);
-        if (implicit) style.import('jeet');
+        if (implicit) style.import('index');
     }
 }
