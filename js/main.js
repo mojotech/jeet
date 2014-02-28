c45a306dfb2bfbeacc38fd88169bdbe73ea095dd
CorySimmons <cory@mojotech.com>
2014-02-28 00:37:24 -0500

    [draft] Nav concept

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
+  $('.content').anchorific();
 
+  $('.anchorific a').click(function(e) {
+    e.preventDefault();
+  });
+
+  $('.anchorific a').each(function(e) {
+    $(this).attr('data-scroll-nav', e);
+  });
+
+  $.scrollIt({
+    activeClass: 'scrollIt-active',
+    topOffset: -20
+  });
 
 });
