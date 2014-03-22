fc9308da29c2eb52d35068720cea6a1b75cdb72f
CorySimmons <cory@mojotech.com>
2014-03-22 04:55:23 -0400

    [cleanup] Remove unused files

--- stylus/Gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-    require('gulp-stylus'),
-    jeet = require('jeet');
-
-gulp.task('jeet', function () {
-    gulp.src('./css/style.styl')
-        .pipe(stylus({use: ['jeet']}))
-        .pipe(gulp.dest('./css/'));
-});
