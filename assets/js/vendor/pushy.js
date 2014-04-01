ab05749e2d5be544297e07e713bb5b811f4984b4
eliwilliamson <eliwilliamson@gmail.com>
2014-04-01 09:29:49 -0400

    minor adjustments to toggle api

--- assets/js/vendor/pushy.js
+++ assets/js/vendor/pushy.js
@@ -10,6 +10,7 @@ $(function() {
     container = $('#demo-container'), //container css class
     push = $('.interactive-api'), //css class to add pushy capability
     pushyClass = "pushy-left pushy-open", //menu position & menu open class
+    pushyActive = "pushy-active",
     containerClass = "container-push", //container open class
     pushClass = "push-push", //css class to add pushy capability
     menuBtn = $('.toggle-api'), //css classes to toggle the api
@@ -17,18 +18,21 @@ $(function() {
     menuWidth = ((body.width() * 0.3) + "px"); //jQuery fallback menu width
 
   function togglePushy(){
+    body.toggleClass(pushyActive);
     pushy.toggleClass(pushyClass);
     container.toggleClass(containerClass);
     push.toggleClass(pushClass); //css class to add pushy capability
   }
 
   function openPushyFallback(){
+    body.addClass(pushyActive);
     pushy.animate({left: "0%"}, menuSpeed);
     container.animate({left: menuWidth}, menuSpeed);
     push.animate({left: menuWidth}, menuSpeed); //css class to add pushy capability
   }
 
   function closePushyFallback(){
+    body.removeClass(pushyActive);
     pushy.animate({left: "-" + menuWidth}, menuSpeed);
     container.animate({left: "0%"}, menuSpeed);
     push.animate({left: "0%"}, menuSpeed); //css class to add pushy capability
@@ -75,6 +79,7 @@ $(function() {
     if (screenResized) {
       if ($(window).width() <= 1024 && !state) {
         if(Modernizr.csstransforms3d){
+          body.removeClass(pushyActive);
           pushy.addClass('pushy-left');
           pushy.removeClass('pushy-open');
           container.removeClass(containerClass);
