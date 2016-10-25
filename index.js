e95edabe1b5f36f65e0e15e040fb5871d6d7e13d
Cory Simmons <cory@launchboxhq.com>
2016-10-25 00:16:28 -0400

    Add option to implicitly import Jeet
    This is handy for `stylus —use jeet` usage and probably other places. It removes the need to include `@import ‘jeet’` at the top of every file.

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ var pkg = require('./package.json');
 var jeetPath = path.join(__dirname, 'styl')
 
 exports = module.exports = function (opts) {
+  var implicit = (opts && opts.implicit == false) ? false : true;
+
   return function (style) {
-    style.import(jeetPath);
+    style.include(__dirname);
+
+    if (implicit) {
+      style.import(jeetPath);
+    }
   }
 }
 
